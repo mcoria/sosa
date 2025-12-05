1790bf950705fd2b46f0913e467e0bc1d48cbb21
Mauricio Coria <coriamauricio@gmail.com>
2025-12-05 10:56:30 -0300

    Refactoring part 6

--- worker/src/main/java/net/chesstango/sosa/worker/lichess/LichessClientBean.java
+++ worker/src/main/java/net/chesstango/sosa/worker/lichess/LichessClientBean.java
@@ -2,8 +2,7 @@ package net.chesstango.sosa.worker.lichess;
 
 import chariot.Client;
 import chariot.ClientAuth;
-import chariot.api.ChallengesApiAuthCommon;
-import chariot.model.*;
+import chariot.model.GameStateEvent;
 import lombok.extern.slf4j.Slf4j;
 import net.chesstango.sosa.worker.events.LichessConnected;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,9 +11,6 @@ import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
-import java.util.Map;
-import java.util.Optional;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 /**
@@ -46,82 +42,7 @@ public class LichessClientBean implements LichessClient {
     }
 
     @Override
-    public Stream<Event> streamEvents() {
-        return imp.streamEvents();
-    }
-
-    @Override
     public Stream<GameStateEvent> streamGameStateEvent(String gameId) {
         return imp.streamGameStateEvent(gameId);
-    }
-
-    @Override
-    public Challenge challenge(User user, Consumer<ChallengesApiAuthCommon.ChallengeBuilder> challengeBuilderConsumer) {
-        return imp.challenge(user, challengeBuilderConsumer);
-    }
-
-    @Override
-    public void challengeAccept(String challengeId) {
-        imp.challengeAccept(challengeId);
-    }
-
-    @Override
-    public void challengeDecline(String challengeId) {
-        imp.challengeDecline(challengeId);
-    }
-
-    @Override
-    public void cancelChallenge(String challengeId) {
-        imp.cancelChallenge(challengeId);
-    }
-
-    @Override
-    public void gameMove(String gameId, String moveUci) {
-        imp.gameMove(gameId, moveUci);
-    }
-
-    @Override
-    public void gameResign(String gameId) {
-        imp.gameResign(gameId);
-    }
-
-    @Override
-    public void gameChat(String gameId, String message) {
-        imp.gameChat(gameId, message);
-    }
-
-    @Override
-    public void gameAbort(String gameId) {
-        imp.gameAbort(gameId);
-    }
-
-    @Override
-    public Map<StatsPerfType, StatsPerf> getRatings() {
-        return imp.getRatings();
-    }
-
-    @Override
-    public int getRating(StatsPerfType type) {
-        return imp.getRating(type);
-    }
-
-    @Override
-    public boolean isMe(UserInfo theUser) {
-        return imp.isMe(theUser);
-    }
-
-    @Override
-    public Many<User> botsOnline() {
-        return imp.botsOnline();
-    }
-
-    @Override
-    public Optional<UserAuth> findUser(String username) {
-        return imp.findUser(username);
-    }
-
-    @Override
-    public Many<GameInfo> meOngoingGames() {
-        return imp.meOngoingGames();
     }
 }
--- worker/src/main/java/net/chesstango/sosa/worker/lichess/LichessClientImp.java
+++ worker/src/main/java/net/chesstango/sosa/worker/lichess/LichessClientImp.java
@@ -1,15 +1,9 @@
 package net.chesstango.sosa.worker.lichess;
 
 import chariot.ClientAuth;
-import chariot.api.ChallengesApiAuthCommon;
-import chariot.model.*;
+import chariot.model.GameStateEvent;
 import lombok.extern.slf4j.Slf4j;
 
-
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import java.util.function.Consumer;
 import java.util.stream.Stream;
 
 /**
@@ -24,102 +18,7 @@ public class LichessClientImp implements LichessClient {
     }
 
     @Override
-    public synchronized Stream<Event> streamEvents() {
-        return client.bot().connect().stream();
-    }
-
-    @Override
     public synchronized Stream<GameStateEvent> streamGameStateEvent(String gameId) {
         return client.bot().connectToGame(gameId).stream();
-    }
-
-    @Override
-    public synchronized Challenge challenge(User user, Consumer<ChallengesApiAuthCommon.ChallengeBuilder> challengeBuilderConsumer) {
-        One<Challenge> optChallenge = client.bot()
-                .challenge(user.id(), challengeBuilderConsumer);
-
-        if(optChallenge.isPresent()){
-            return optChallenge.get();
-        } else {
-            throw new RuntimeException("Error sending challenge to " + user.id());
-        }
-    }
-
-    @Override
-    public synchronized void challengeAccept(String challengeId) {
-        client.bot().acceptChallenge(challengeId);
-    }
-
-    @Override
-    public synchronized void challengeDecline(String challengeId) {
-        client.bot().declineChallenge(challengeId);
-    }
-
-    @Override
-    public synchronized void cancelChallenge(String challengeId) {
-        client.bot().cancelChallenge(challengeId);
-    }
-
-    @Override
-    public synchronized void gameMove(String gameId, String moveUci) {
-        client.bot().move(gameId, moveUci);
-    }
-
-    @Override
-    public synchronized void gameResign(String gameId) {
-        client.bot().resign(gameId);
-    }
-
-    @Override
-    public synchronized void gameChat(String gameId, String message) {
-        client.bot().chat(gameId, message);
-    }
-
-    @Override
-    public synchronized void gameAbort(String gameId) {
-        client.bot().abort(gameId);
-    }
-
-    @Override
-    public synchronized Map<StatsPerfType, StatsPerf> getRatings() {
-        return client.account()
-                .profile()
-                .get()
-                .ratings();
-    }
-
-    @Override
-    public synchronized int getRating(StatsPerfType type) {
-        Map<StatsPerfType, StatsPerf> rating = client.account()
-                .profile()
-                .get()
-                .ratings();
-        StatsPerf stats = rating.get(type);
-        if (stats instanceof StatsPerf.StatsPerfGame statsPerfGame) {
-            return statsPerfGame.rating();
-        }
-        throw new RuntimeException("Rating not found");
-    }
-
-    @Override
-    public synchronized boolean isMe(UserInfo theUser) {
-        return client.account().profile().get().id().equals(theUser.id());
-    }
-
-    @Override
-    public synchronized Many<User> botsOnline() {
-        return client.bot().botsOnline();
-    }
-
-    @Override
-    public synchronized Optional<UserAuth> findUser(String username) {
-        Many<UserAuth> users = client.users().byIds(List.of(username));
-        return users.stream().findFirst();
-    }
-
-
-    @Override
-    public synchronized Many<GameInfo> meOngoingGames() {
-        return client.games().ongoing();
     }
 }
