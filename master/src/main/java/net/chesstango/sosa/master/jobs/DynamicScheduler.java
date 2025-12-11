9ded9e5b7a5f878e2c5f9f39aaa9f221b9fb97c7
Mauricio Coria <coriamauricio@gmail.com>
2025-12-11 08:34:13 -0300

    Adding handling for `LichessTooManyRequestsSent` event and improving dynamic scheduler logic

--- master/src/main/java/net/chesstango/sosa/master/jobs/DynamicScheduler.java
+++ master/src/main/java/net/chesstango/sosa/master/jobs/DynamicScheduler.java
@@ -3,6 +3,7 @@ package net.chesstango.sosa.master.jobs;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import net.chesstango.sosa.master.events.LichessTooManyGamesPlayed;
+import net.chesstango.sosa.master.events.LichessTooManyRequestsSent;
 import net.chesstango.sosa.master.lichess.errors.RetryIn;
 import org.quartz.*;
 import org.springframework.beans.factory.annotation.Value;
@@ -38,12 +39,8 @@ public class DynamicScheduler {
     @EventListener
     public void onLichessTooManyGamesPlayed(LichessTooManyGamesPlayed lichessTooManyGamesPlayed) {
         log.warn("Lichess API: too many games. Stop sending challenges to lichess.");
-        schedulePublishExpiredTimerEventJob(lichessTooManyGamesPlayed.getRetryIn());
-    }
-
-
-    void schedulePublishExpiredTimerEventJob(RetryIn retryIn) {
         try {
+            RetryIn retryIn = lichessTooManyGamesPlayed.getRetryIn();
             JobDetail job = JobBuilder.newJob(PublishExpiredTimerEvent.class)
                     .withIdentity("PublishTooManyExpiredTimerEvent_Game")
                     .usingJobData("expirationType", "GAMES")
@@ -61,4 +58,27 @@ public class DynamicScheduler {
             throw new RuntimeException(e);
         }
     }
+
+    @EventListener(LichessTooManyRequestsSent.class)
+    public void onLichessTooManyRequestsSent() {
+        log.error("Lichess API: too many requests. Stop sending requests to lichess for a while.");
+        try {
+            JobDetail job = JobBuilder.newJob(PublishExpiredTimerEvent.class)
+                    .withIdentity("PublishTooManyExpiredTimerEvent_Requests")
+                    .usingJobData("expirationType", "GAMES")
+                    .storeDurably()
+                    .build();
+
+            Trigger trigger = TriggerBuilder.newTrigger()
+                    .withIdentity("PublishTooManyExpiredTimerEventTrigger_Requests")
+                    .startAt(DateBuilder.futureDate(90, DateBuilder.IntervalUnit.SECOND))
+                    .build();
+
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException e) {
+            log.error("Error scheduling:", e);
+            throw new RuntimeException(e);
+        }
+    }
+
 }
