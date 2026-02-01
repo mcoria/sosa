299ed677d22da73cb1a000d7acad50ea81950dec
Mauricio Coria <coriamauricio@gmail.com>
2026-02-01 09:12:56 -0300

    Refactoring BatchConfig and job setup: migrating to `@EnableBatchProcessing`, removing redundant beans, updating tasklet signatures, and adding `jackson-databind` dependency.

--- worker-init/src/main/java/net/chesstango/sosa/init/configs/RabbitConfig.java
+++ worker-init/src/main/java/net/chesstango/sosa/init/configs/RabbitConfig.java
@@ -1,6 +1,5 @@
 package net.chesstango.sosa.init.configs;
 
-import net.chesstango.sosa.messages.Constants;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
@@ -9,7 +8,6 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- worker-init/src/main/java/net/chesstango/sosa/init/steps/WaitGameStart.java
+++ worker-init/src/main/java/net/chesstango/sosa/init/steps/WaitGameStart.java
@@ -6,12 +6,14 @@ import net.chesstango.sosa.messages.worker.GameStart;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.batch.core.ExitStatus;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.StepExecutionListener;
+
+import org.springframework.batch.core.listener.StepExecutionListener;
 import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.StepContribution;
+import org.springframework.batch.core.step.StepExecution;
 import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
+
+import org.springframework.batch.infrastructure.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
 /**
